7ff52b9fb9def613da69c9982c71150993d17f24
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-01 02:11:41 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,16 +4,24 @@ function Hero() {
       <div className="container-wrapper">
         <div className="relative z-50">
           <h4 className="mt-4 text-2xl text-center font-ninja text-[#a1ff3c]">
-          Samuria Panda : The Legendary Solana Sensation! 
+            Samuria Panda : The Legendary Solana Sensation!
           </h4>
 
           <div className=" flex justify-between flex-col sm:flex-row items-center ">
             <div className=" w-full">
-              <h1 className=" font-ninja text-5xl my-4">  Samuria Panda </h1>
+              <h1 className=" font-ninja text-5xl my-4"> Samuria Panda </h1>
 
-              <p className=" text-base sm:text-xl">Hop on board, fellow meme enthusiasts and crypto adventurers! Welcome to the realm of  Samuria Panda , the ultimate fighter Panda of the Solana blockchain!</p>
+              <p className=" text-base sm:text-xl">
+                Welcome to the whimsical world of Samurai Panda, where cuteness
+                meets warrior spirit, and memes collide with trading prowess.
+                Join us as a lovable panda leads a community on a mission to
+                revolutionize the Solana meme world.
+              </p>
               <p className=" text-base sm:text-xl py-2">
-                Samuria Panda  isn't just your average meme; it's a force to be reckoned with! With lightning speed, unbeatable strength, boundless energy, and a charm that'll make even the toughest whales swoon,  Samuria Panda  is here to take the Solana community to the moon and beyond!
+              Samurai Panda Power combines Meme with Profit! With lightning speed, unbeatable strength,
+                boundless energy, and a charm that'll make even the toughest
+                whales swoon, Samuria Panda is here to take the Solana community
+                to the moon and beyond!
               </p>
               <div className="relative z-40 flex items-center justify-start my-4 max-sm:flex-col max-sm:space-y-4 sm:space-x-5">
                 <a
@@ -36,8 +44,6 @@ function Hero() {
               <div>
                 <img src="images/hero.png" alt="" className=" mx-auto" />
               </div>
-
-         
             </div>
           </div>
         </div>
